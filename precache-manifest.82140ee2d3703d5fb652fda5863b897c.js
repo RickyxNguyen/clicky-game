36b26fb20e02b402ce3364ef642ec07902a2e7b8
Ricky Nguyen <ricky.nguyen@me.com>
2019-01-15 20:00:07 -0600

    Updates

---
+++ precache-manifest.82140ee2d3703d5fb652fda5863b897c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6acb6692e4ee25941da6",
+    "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
+  },
+  {
+    "revision": "4a08e4466e787d75e69e",
+    "url": "/clicky-game/static/js/main.4a08e446.chunk.js"
+  },
+  {
+    "revision": "9c49b1657f794805e3dd",
+    "url": "/clicky-game/static/js/1.9c49b165.chunk.js"
+  },
+  {
+    "revision": "4a08e4466e787d75e69e",
+    "url": "/clicky-game/static/css/main.89fcdcf3.chunk.css"
+  },
+  {
+    "revision": "e0880e8e22297464c1421061a8ddba8e",
+    "url": "/clicky-game/index.html"
+  }
+];
